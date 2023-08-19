f17857bd18b54568402d69b0a8873c60d3788a44
Championshi <135168416+Championshi@users.noreply.github.com>
2023-08-19 21:44:34 +0530

    Add files via upload

---
+++ city.js
@@ -0,0 +1,39 @@
+export const city = [ 
+    {
+        name: "Lower Parel",
+        place: 383,
+    },
+    {
+        name: "Patna Junction",
+        place: 355,
+    },
+    {
+        name: "Gandhi Maidan",
+        place: 432,
+    },
+    {
+        name: "Kankar Bagh",
+        place: 93,
+    },
+    {
+        name: "Boring Road",
+        place: 220,
+    },
+    {
+        name: "Patna City",
+        place: 186,
+    },
+    {
+        name: "Rajendra nagar",
+        place: 34,
+    },
+    {
+        name: "Danapur",
+        place: 67,
+    },
+    {
+        name: "see more",
+        place: "",
+        icon: "fa-solid fa-angle-right"
+    }
+]
---
+++ index.js
@@ -0,0 +1,17 @@
+import { city } from "./city.js";
+
+
+let localitiesContainerEl = document.querySelector(".localities_container")
+let htmllocalitieslist = city.map((el) => {
+  return `
+      <div class="localities_list">
+         <div class="localities_card">
+            <h3>${el.name}</h3>
+            <p>${el.place} places</p>
+         </div>
+         <i class="fa-solid fa-angle-right"></i>
+      </div >
+      `
+}).join("")
+
+localitiesContainerEl.innerHTML = htmllocalitieslist 
